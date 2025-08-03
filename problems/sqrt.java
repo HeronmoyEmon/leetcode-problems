24a8faf8d532345bf6a1347ce7c87232f7bf0f3e
Heronmoy <heronmoy.cuet.eee@gmail.com>
2025-08-03 13:16:51 +0600

    sqrt problem solved in Java

---
+++ problems/sqrt.java
@@ -0,0 +1,46 @@
+// Problem 69: Sqrt(x)
+// https://leetcode.com/problems/sqrtx/
+//
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+// The returned integer should be non-negative as well.
+//
+// You must not use any built-in exponent function or operator.
+// For example, do not use pow(x, 0.5) in code.
+//
+// Example 1:
+// Input: x = 4
+// Output: 2
+//
+// Example 2:
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we want the floor of the square root, we return 2.
+
+
+class Solution {
+    public int mySqrt(long x) {
+        if (x<2) {
+            return (int) x;
+        }
+
+        long min = 1;
+        long max = x/2;
+        int result = 1;
+
+        while (min <= max) {
+            long mid = (min + max) / 2;
+            long square = mid * mid;
+
+            if (square == x) {
+                return (int) mid;
+            } else if (square > x) {
+                max = mid - 1;
+            } else {
+                result = (int) mid;
+                min = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
